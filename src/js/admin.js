a36b473192dd9a457f113d6555121c4594a04737
Jonathan Plodzien <joonyyplo@gmail.com>
2020-07-15 10:04:05 -0300

    se creo funcion para tibujar la tabla y agregar los productos al local storage

--- src/js/admin.js
+++ src/js/admin.js
@@ -4,22 +4,91 @@ import "../css/style.css";
 import Funko from "./funko";
 
 let listaProductos = [];
+leerDatos();
 
-function agregarProducto() {
-  let codigo = document.getElementById("codigo").value,
-    nombre = document.getElementById("nombre").value,
-    numSerie = document.getElementById("numSerie").value,
-    descripcion = document.getElementById("descripcion").value,
-    categoria = document.getElementById("categoria").value,
-    imagen = document.getElementById("imagen").value;
+window.agregarProducto = function (e) {
+  e.preventDefault();
+  console.log("en la funcion");
+  let codigo = document.getElementById("codigo"),
+    nombre = document.getElementById("nombre"),
+    numSerie = document.getElementById("numSerie"),
+    descripcion = document.getElementById("descripcion"),
+    categoria = document.getElementById("categoria"),
+    imagen = document.getElementById("imagen");
   //Validar formulario
+  if (
+    revisarCodigo(codigo) &&
+    requerido(nombre) &&
+    requerido(numSerie) &&
+    requerido(descripcion) &&
+    requerido(categoria) &&
+    requerido(imagen)
+  ) {
+    let producto = new Funko(
+      codigo.value,
+      nombre.value,
+      numSerie.value,
+      categoria.value,
+      descripcion.value,
+      imagen.value
+    );
+    listaProductos.push(producto);
+    localStorage.setItem("funkoKey", JSON.stringify(listaProductos));
+  } else {
+    alert("Faltan completar datos");
+  }
+};
 
-  let producto = new Funko(
-    codigo,
-    nombre,
-    numSerie,
-    categoria,
-    descripcion,
-    imagen
-  );
+window.revisarCodigo = function (checkCodigo) {
+  if (checkCodigo.value != "" && !isNaN(checkCodigo.value)) {
+    console.log(checkCodigo);
+    checkCodigo.className = "form-control is-valid";
+    return true;
+  } else {
+    checkCodigo.className = "form-control is-invalid";
+    return false;
+  }
+};
+
+window.requerido = function (checkNombre) {
+  if (checkNombre.value != "") {
+    checkNombre.className = "form-control is-valid";
+    return true;
+  } else {
+    checkNombre.className = "form-control is-invalid";
+    return false;
+  }
+};
+
+function leerDatos() {
+  if (localStorage.length > 0) {
+    let arregloLS = JSON.parse(localStorage.getItem("funkoKey"));
+    if (listaProductos.length == 0) {
+      listaProductos = arregloLS;
+    }
+    //Borrar filas
+    //Dibujar las filas de la tabla
+    dibujarFilas(arregloLS);
+  }
+}
+
+function dibujarFilas(arregloLS) {
+  let tbody = document.getElementById("tablaProducto");
+  let codigoHTML = "";
+
+  for (let i in arregloLS){
+    codigoHTML = `<tr>
+                        <th scope="row">${arregloLS[i].codigo}</th>
+                        <td>${arregloLS[i].nombre}</td>
+                        <td>${arregloLS[i].numSerie}</td>
+                        <td>${arregloLS[i].categoria}</td>
+                        <td>${arregloLS[i].descripcion}</td>
+                        <td>${arregloLS[i].imagen}.png</td>
+                        <td>
+                            <button class="btn btn-outline-info">Modificar</button>
+                            <button class="btn btn-outline-danger">Eliminar</button>
+                        </td>
+                    </tr>`;
+    tbody.innerHTML += codigoHTML;
+  }
 }
