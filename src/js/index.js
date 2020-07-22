b2d878f76f482fd6d16c0792418e2a2ba96ed1db
Delfi <delfimassa@gmail.com>
2020-07-22 10:19:48 -0300

    se agrego la logica que permite dar de alta los productos desde la web

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/style.css';
+leerDatos();
+function leerDatos() {
+    if (localStorage.length > 0) {
+        let arregloLS = JSON.parse(localStorage.getItem("funkoKey"));
+        let article = document.getElementById("article");
 
-let saludo = "comision 1A"
-console.log("desde el archivo index.js "+saludo)
+        for (let i in arregloLS) {
+            let codigoHTML = `<div class="col-sm-12 col-md-4 col-lg-3">
+            <div class="card">
+                <img src="img/productos/${arregloLS[i].imagen}" class="card-img-top" alt="${arregloLS[i].nombre}">
+                <div class="card-body">
+                    <h5 class="card-title">${arregloLS[i].nombre}</h5>
+                    <p class="card-text">${arregloLS[i].descripcion}</p>
+                    <a href="#" class="btn btn-primary disabled">Comprar</a>
+                </div>
+            </div>
+            </div>`
+
+            article.innerHTML += codigoHTML;
+        }
+    }
+}
